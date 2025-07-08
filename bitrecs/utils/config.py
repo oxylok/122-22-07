1be8f23763c14fc5358f3b8dc69ab722b85b0b15
dmose <100089505+janusdotai@users.noreply.github.com>
2025-07-08 15:37:10 -0400

    epoch length default

--- bitrecs/utils/config.py
+++ bitrecs/utils/config.py
@@ -85,7 +85,7 @@ def add_args(cls, parser):
         "--neuron.epoch_length",
         type=int,
         help="The default epoch length (how often we set weights, measured in 12 second blocks).",
-        default=500,
+        default=200,
     )
 
     parser.add_argument(
