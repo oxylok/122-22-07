d193c77d8057368f30454143cd7c832c8d627b94
dmose <100089505+janusdotai@users.noreply.github.com>
2025-07-19 11:31:09 -0400

    low coverage warning

--- bitrecs/base/validator.py
+++ bitrecs/base/validator.py
@@ -643,11 +643,12 @@ class BaseValidatorNeuron(BaseNeuron):
             bt.logging.warning("Scores are all zero.")
             return        
       
-        # coverage = (len(self.batch_seen_uids) / len(self.total_uids)) * 100 if len(self.total_uids) > 0 else 0
-        # min_coverage = 0.9
-        # if coverage < min_coverage:
-        #     bt.logging.warning(f"\033[3;3mCoverage {coverage:.2f}% is below minimum threshold of {min_coverage * 100:.2f}%. Skipping weight update. \033[0m")
-        #     return
+        coverage = (len(self.batch_seen_uids) / len(self.total_uids)) * 100 if len(self.total_uids) > 0 else 0
+        min_coverage = 0.9
+        if coverage < min_coverage:
+            bt.logging.warning(f"\033[3;3mUpdating premature weights! \033[0m")
+            bt.logging.warning(f"\033[3;3mCoverage {coverage:.2f}% is below minimum threshold of {min_coverage * 100:.2f}%.\033[0m")
+            #return
 
         # Use normalized scores for weights
         raw_weights = self.get_normalized_scores()
